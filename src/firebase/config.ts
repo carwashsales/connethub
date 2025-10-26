4e8001e82f3987f18d0433c6e4ad9cd3c6d19d34
KAWOOYA RAYMOND <kawooyaraymond40@gmail.com>
2025-10-26 09:53:57 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,8 @@
-// This file is intentionally left blank. 
-// The Firebase config will be populated later.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  "projectId": "studio-2028468172-bbafb",
+  "appId": "1:253953287545:web:909d5342b375227a0bbba8",
+  "apiKey": "AIzaSyAz3D0luuPXsD079dTfwHBowNhnfmBp3to",
+  "authDomain": "studio-2028468172-bbafb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "253953287545"
+};
